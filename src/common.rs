906dc571499e4cc569665fbbc0908fe40a2ac9b3
yuanleezx <113503387+yuanleezx@users.noreply.github.com>
2025-11-19 01:52:49 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -1055,7 +1055,7 @@ fn get_api_server_(api: String, custom: String) -> String {
 
 #[inline]
 pub fn is_public(url: &str) -> bool {
-    url.contains("rustdesk.com")
+    url.contains("10.107.10.50")
 }
 
 pub fn get_udp_punch_enabled() -> bool {
